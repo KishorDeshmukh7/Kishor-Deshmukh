4930b873845981d40e29c3902d22b33abff7d156
DELL <DELL@DESKTOP-Q5VHMF9>
2022-08-25 13:43:40 +0530

    jira- 1234 Description project Kishor Deshmukh

---
+++ Java Practice/src/practice/Job_laguDe.java
@@ -0,0 +1,23 @@
+package practice;
+
+
+public class Job_laguDe {
+	
+	public static void main(String[] args) {
+ 		int a = 6;
+		int result = 0;
+		for(int i = a; i>=1; i--)
+			
+			result = result*i;
+	
+		System.out.println(result);
+		
+	}
+	
+	
+	}
+
+	 
+	
+	
+	      
---
+++ Java Practice/src/practice/k9ishor.java
@@ -0,0 +1,5 @@
+package practice;
+
+public interface k9ishor {
+
+}
